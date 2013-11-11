711943cff692cbcd0a81657018e9e0177ce7e7dd
Audrey Carlsen <audreycarlsen@gmail.com>
2013-11-11 14:42:44 -0800

    Added market.rb file

--- lib/far_mar_finder.rb
+++ lib/far_mar_finder.rb
@@ -4,5 +4,17 @@ require_relative 'market'
 # ... Require all of the supporting classes
 
 class FarMarFinder
-  # Your code goes here
+  
 end
+
+finder = FarMarFinder.new
+finder.markets
+ #=> Market
+finder.vendors
+ #=> Vendor
+finder.products
+ #=> Product
+finder.sales
+ #=> Sale
+finder.markets.all
+# => [...] Returns all instances of the Market class
