a47ac8b122d2581927a7a9421237e72d7a64a1c1
Sue White <sue@mailworks.org>
2013-11-11 16:35:04 -0800

    Added finding methods to market class

--- lib/market.rb
+++ lib/market.rb
@@ -9,11 +9,33 @@ class Market
     @county = ""
     @state = ""
     @zip = ""
+    @market_data = CSV.read('./support/markets.csv')
+    @index_hash = {:id => 0, :name => 1, :address => 2, :city => 3, :county => 4, :state => 5, :zip => 6}
   end
 
   def self.all
-    CSV.read("./support/markets.csv").map do |array|
+    @market_data.each do |array|
       Market.new(array)
     end
   end
+
+  def self.find(id)
+    @market_data.find do |array|
+      array[0] == id
+    end
+  end
+
+  def self.find_by(x, match)
+    @market_data.find do |array|
+      array[@index_hash[x.to_sym]] == /#{match}/i
+    end
+  end
+
+  def self.find_all_by(x, match)
+    @market_data.select do |array|
+      array[@index_hash[x.to_sym]] == /#{match}/i
+    end
+  end
+
+  # TODO check for x's validity
 end
