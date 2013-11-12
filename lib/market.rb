4ac7c42704694dc9e08210fa0ed409578220fad3
Audrey Carlsen <audreycarlsen@gmail.com>
2013-11-11 20:30:37 -0800

    Added values for instance variables in Vendor and Market classes, so that it reads attributes from the csv files

--- lib/market.rb
+++ lib/market.rb
@@ -2,39 +2,41 @@ class Market
   attr_accessor :id, :name, :address, :city, :county, :state, :zip
 
   def initialize(array)
-    @id = ""
-    @name = ""
-    @address = ""
-    @city = ""
-    @county = ""
-    @state = ""
-    @zip = ""
-    @market_data = CSV.read('./support/markets.csv')
-    @index_hash = {:id => 0, :name => 1, :address => 2, :city => 3, :county => 4, :state => 5, :zip => 6}
+    @id = array[0]
+    @name = array[1]
+    @address = array[2]
+    @city = array[3]
+    @county = array[4]
+    @state = array[5]
+    @zip = array[6]
   end
 
   def self.all
-    @market_data.each do |array|
+    CSV.read('./support/markets.csv').each.map do |array|
       Market.new(array)
     end
   end
 
   def self.find(id)
-    @market_data.find do |array|
-      array[0] == id
+    all.find do |market|
+      market.id
     end
   end
 
   def self.find_by(x, match)
-    @market_data.find do |array|
-      array[@index_hash[x.to_sym]] == /#{match}/i
+    all.find do |market|
+      market.x.downcase
     end
   end
 
   def self.find_all_by(x, match)
-    @market_data.select do |array|
-      array[@index_hash[x.to_sym]] == /#{match}/i
+    all.select do |market|
+      market.x.downcase
     end
+  end
+
+  def vendors
+
   end
 
   # TODO check for x's validity
--- lib/vendor.rb
+++ lib/vendor.rb
@@ -2,15 +2,45 @@ class Vendor
   attr_accessor :id, :name, :num_employees, :market_id
 
   def initialize(array)
-    @id = ""
-    @name = ""
-    @num_employees = ""
-    @market_id = ""
+    @id = array[0]
+    @name = array[1]
+    @num_employees = array[2]
+    @market_id = array[3]
   end
 
   def self.all
-    CSV.read("./support/vendors.csv").map do |array|
+    CSV.read('./support/vendors.csv').each.map do |array|
       Vendor.new(array)
     end
+  end
+
+  def self.find(id)
+    all.find do |vendor|
+      vendor.id
+    end
+  end
+
+  def self.find_by(x, match)
+    all.find do |vendor|
+      vendor.x.downcase
+    end
+  end
+
+  def self.find_all_by(x, match)
+    all.select do |vendor|
+      vendor.x.downcase
+    end
+  end
+
+  def market
+  end
+
+  def products
+  end
+
+  def sales
+  end
+
+  def revenue
   end
 end
